9c6b8aee55bc93fb1746d05eea9673bb95873449
Gregory Wild-Smith <gwildsmith@bleacherreport.com>
2017-12-24 18:23:56 -0800

    Breaking main file out into modular code

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 /* eslint no-global-assign: 0 no-native-reassign: 0 */
 require = require("@std/esm")(module);
-module.exports = require("./docco.js").default;
+module.exports = require("./lib/docco.js").default;
